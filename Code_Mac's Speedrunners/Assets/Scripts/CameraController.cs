ce7b4e26d3d93fde38e6b312277b5f758d120b98
Mac <CodeMac.gamedev@gmail.com>
2022-02-07 22:38:01 +0100

    fixed camera

---
+++ Code_Mac's Speedrunners/Assets/Scripts/CameraController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Parent class for camera handeling.
+public class CameraController : MonoBehaviour
+{
+
+}
--- Code_Mac's Speedrunners/Assets/Scripts/PlayerMouse.cs
+++ Code_Mac's Speedrunners/Assets/Scripts/PlayerMouse.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMouse : MonoBehaviour
+public class PlayerMouse : CameraController
 {
     public float mouseXSensitivity = 1f;
     public float mouseYSensitivity = 1f;
@@ -10,6 +10,10 @@ public class PlayerMouse : MonoBehaviour
     float mouseX = 0;
     float mouseY = 0;
     private float xRotation = 0f;
+
+    public float backDistance;
+
+    public Transform mainCamera;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,10 +33,31 @@ public class PlayerMouse : MonoBehaviour
         }*/
         
         xRotation -= axisMouseY;
-        xRotation = Mathf.Clamp(xRotation, -42, 48);
+        xRotation = Mathf.Clamp(xRotation, -48, 48);
         rotationPoint.localRotation = Quaternion.Euler(xRotation, 0, 0);
         //Debug.Log("xRotation:" + xRotation + " mouseY:" + axisMouseY);
         Vector3 rotation = new Vector3(xRotation, mouseX, 0);
         rotationPoint.eulerAngles = rotation;
+
+        Vector3 tempDirection = mainCamera.position - transform.position ;
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position + Vector3.Normalize(tempDirection), tempDirection, out hit, 4f))
+        {
+            Debug.Log(hit.point);
+           mainCamera.position = hit.point - Vector3.Normalize(tempDirection) *  0.05f;
+        }
+
+    }
+    void OnDrawGizmosSelected()
+    {
+            // Draws a blue line from this transform to the target
+            Gizmos.color = Color.red;
+        Vector3 tempDirection =  mainCamera.position - transform.position ;
+        Gizmos.DrawLine(transform.position + Vector3.Normalize(tempDirection), transform.position + tempDirection * 100f);
+
+        // Gizmos.DrawLine(transform.position, transform.position - tempDirection * 100);
+
+
     }
 }
